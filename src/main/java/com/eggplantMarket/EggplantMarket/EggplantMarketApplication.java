4d717dd91bb72783e207636efb77c3553e980183
HMWG <nurung22222@gmail.com>
2022-05-12 00:34:35 +0900

    First Commit

---
+++ src/main/java/com/eggplantMarket/EggplantMarket/EggplantMarketApplication.java
@@ -0,0 +1,13 @@
+package com.eggplantMarket.EggplantMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggplantMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggplantMarketApplication.class, args);
+	}
+
+}
